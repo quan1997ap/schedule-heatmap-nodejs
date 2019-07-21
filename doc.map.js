f66f75f86fbea2ffcb5c614d639ce2c4d5170260
quan1997ap <quan1997ap@gmail.com>
2019-07-21 10:20:29 +0700

    gitignore

--- doc.map.js
+++
@@ -1,4 +0,0 @@
-// https://nominatim.openstreetmap.org/search.php?q=th%C3%A0nh+ph%E1%BB%91+h%E1%BB%93+ch%C3%AD+minh+viet+nam&polygon_geojson=1&viewbox=
-// https://nominatim.openstreetmap.org/search.php?q=th%C3%A0nh+ph%E1%BB%91+h%E1%BB%93+ch%C3%AD+minh+viet+nam&polygon_geojson=1&format=json
-// so sanh các phuong phap noi suy 
-// https://ars.els-cdn.com/content/image/1-s2.0-S1110982313000276-gr2.jpg 
