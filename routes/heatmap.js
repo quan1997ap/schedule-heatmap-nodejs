7758e762d9fe663c15b627780fd7310b98c2d20b
quan1997ap <quan1997ap@gmail.com>
2019-07-23 16:48:20 +0700

    reconnect to db

--- routes/heatmap.js
+++ routes/heatmap.js
@@ -28,20 +28,12 @@ router.get("/", (request, response) => {
 
 router.get("/current-day", (request, response) => {
   rethinkdbdash.r.table('heatmaps')
-  // .filter(
-  //   r.row('date').during(
-  //     r.epochTime(1512864000000 / 1000), 
-  //     r.epochTime(1513209600000 / 1000),
-  //     {leftBound: "open", rightBound: "closed"}
-  //   )
-  // )
   .run(rethinkdbdash.getConnection(), (err, res) => {
     if (err) {
       response.json({
         success: false,
         code: 500
       });
-      console.log('c');
     } else {
       {
         // const resJson = JSON.parse(CircularJSON.stringify(res))._responses;
